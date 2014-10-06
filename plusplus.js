dee95e067981c7980afc5c1b8a79a200907a9d38
Tyler Smith <tylsmith@gmail.com>
2014-10-06 11:28:35 -0700

    basically leaderboard

--- plusplus.js
+++ plusplus.js
@@ -2,10 +2,46 @@ if (Meteor.isClient) {
   Template.router.loggedIn = function(){
     return !!Meteor.user();
   }
+
+  Template.home.plusems = function(){
+    return Plusems.find();
+  }
+
+  Template.home.events({
+    'click #plus': function () {
+      Meteor.call('plus', this)
+    },
+    'click #minus': function () {
+      Meteor.call('minus', this)
+    },
+    'click #temp-create-handle': function () {
+      var handle = document.getElementById("temp-handle").value;
+      Meteor.call("add", handle);
+    }
+  })
+
+  Accounts.ui.config({
+   passwordSignupFields: 'USERNAME_ONLY'
+  });
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+
+  Meteor.methods({
+    plus: function (handle) {
+      Plusems.update(handle, {$inc: {points: 1}});
+    },
+    minus: function (handle) {
+      Plusems.update(handle, {$inc: {points: -1}});
+    },
+    add: function(handle) {
+      Plusems.upsert({handle:handle}, {$set: {points:0}})
+    }
+  });
 }
+
+Plusems = new Mongo.Collection("plusem");
